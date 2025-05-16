76c298472adfd356f29c61a4068c20ad56fb3aaf
Amhar <121789118+qolbyamardepa@users.noreply.github.com>
2025-05-17 00:15:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
